92e1f990025ec1cca6ab42866a69b8c6783b4cb1
David Owino <daveowino753@gmail.com>
2023-03-27 09:29:44 +0300

    Add files via upload

---
+++ prog2/bin/prog2.dart
@@ -0,0 +1,5 @@
+import 'package:prog2/prog2.dart' as prog2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${prog2.calculate()}!');
+}
---
+++ prog2/lib/tetris.dart
@@ -0,0 +1,8 @@
+library tetris;
+
+import 'dart:html';
+import 'dart:async';
+import 'dart:math';
+
+//connect tetris and tile files
+part 'src/model/tile.dart';
